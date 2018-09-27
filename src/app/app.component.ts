7be6018863430e783e935ff397ab1fe930ee2fd5
vaniavc <vania@bagubagu.com>
2018-09-27 18:22:53 +0700

    background & text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,49 +11,54 @@ export class AppComponent implements OnInit {
   step = 0;
 
   ngOnInit() {
-    setInterval(_ => {
-      this.btnNext();
+    setInterval(interval => {
+      this.next();
     }, 5000);
   }
 
-  btnNext() {
+  next() {
     console.log('DOR');
     const boxes = document.getElementsByClassName('boxes') as any;
     const box = document.getElementsByClassName('box');
     const background = document.getElementsByClassName('container') as any;
     const text = document.getElementsByClassName('text');
+    const bg = document.getElementsByClassName('bg');
+
+    // console.log('bg', bg);
+    // console.log('box', box);
 
     if (this.step === 0) {
-      boxes[0].style.left = '0%';
+      console.log('-100', this.step);
+      boxes[0].style.left = '-100%';
       this.step = 1;
       box[0].classList.remove('show');
       box[1].classList.add('show');
       text[0].innerHTML = 'Menganalisa data pribadi anda ...';
+      text[0].classList.remove('fadeout');
       text[0].classList.add('fadein');
-      background[0].style.backgroundImage =
-        'linear-gradient(to right, #37CFDD, #5680DA)';
+      bg[0].classList.remove('show');
+      bg[1].classList.add('show');
     } else if (this.step === 1) {
-      boxes[0].style.left = '-75%';
+      console.log('-200', this.step);
+      boxes[0].style.left = '-200%';
       box[1].classList.remove('show');
       box[2].classList.add('show');
-      text[0].innerHTML = 'Menunggu konfirmasi dari sistem kami ...';
       text[0].classList.remove('fadein');
       text[0].classList.add('fadeout');
-      background[0].style.backgroundImage =
-        'linear-gradient(to right, #8C55E9, #4B74E7)';
+      text[0].innerHTML = 'Menunggu konfirmasi dari sistem kami ...';
+      bg[1].classList.remove('show');
+      bg[2].classList.add('show');
       this.step = 2;
     } else if (this.step === 2) {
-      boxes[0].style.left = '75%';
+      console.log('0', this.step);
+      boxes[0].style.left = '0%';
       box[2].classList.remove('show');
       box[0].classList.add('show');
       text[0].innerHTML =
         'Kami sedang mengecek data anda, silahkan menunggu ...';
-      text[0].classList.remove('fadeout');
-      background[0].style.backgroundImage =
-        'linear-gradient(to right, #43cda3, #569bdd)';
+      bg[2].classList.remove('show');
+      bg[0].classList.add('show');
       this.step = 0;
     }
-
-    // console.log(text);
   }
 }
